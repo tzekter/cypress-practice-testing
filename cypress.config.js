6f356990d52a5b2af0412c0a3182a496a951da0b
Tetiana Zekter <tetianazekter@gmail.com>
2026-09-01 20:46:08 +0300

    Add spec console reporter and Mochawesome HTML reporter configuration

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,16 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
+  reporter: 'cypress-multi-reporters',
+  reporterOptions:{
+    reporterEnabled: 'spec, mochawesome',
+    mochawesomeReporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: true,
+      json: true,
+    },
+  },
 
   e2e: {
     baseUrl: 'https://practicesoftwaretesting.com',
