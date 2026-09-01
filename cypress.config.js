441c27fe6f004f28ceaf50c67ff732b283e59b71
Tetiana Zekter <tetianazekter@gmail.com>
2026-09-01 21:15:50 +0300

    Configure multi-reporters correctly inside e2e block

--- cypress.config.js
+++ cypress.config.js
@@ -2,19 +2,20 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
-  reporter: 'cypress-multi-reporters',
-  reporterOptions:{
-    reporterEnabled: 'spec, mochawesome',
-    mochawesomeReporterOptions: {
-      reportDir: 'cypress/results',
-      overwrite: false,
-      html: true,
-      json: true,
+  
+  e2e: {
+    baseUrl: 'https://practicesoftwaretesting.com',
+    reporter: 'cypress-multi-reporters',
+    reporterOptions:{
+      reporterEnabled: 'spec, mochawesome',
+      mochawesomeReporterOptions: {
+        reportDir: 'cypress/results',
+        overwrite: false,
+        html: true,
+        json: true,
     },
   },
 
-  e2e: {
-    baseUrl: 'https://practicesoftwaretesting.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
